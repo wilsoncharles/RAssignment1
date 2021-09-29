d2f5abf4989867a19c1427eb2390cf7d52b10cc3
wilsoncharles <wilsoncharles21@gmail.com>
2021-09-29 10:45:08 +0100

    2nd Commit on function

--- RAssignment1.R
+++ RAssignment1.R
@@ -1,1 +1,30 @@
-a<-("Wilson first file")
+##Function to calcuate the sum that are divisible by 2 or 7
+myFirstRFunc <-function(num)
+{
+  
+  #Initiating Variable with value 0
+  a=0
+  
+  #For loop to sum number from 1 until n-1  
+  for(i in 1:num-1)
+  {
+    
+    #Conditioning to accept number that are divisible by 2
+    if(i%%2==0)
+    {
+      b=i
+      a=a+b
+    }
+    
+    #Conditioning to accept number that are divisible by 7
+    else if(i%%7==0)
+    {
+      b=i
+      a=a+b
+    }
+  }
+  #Returing Result
+  return(a)
+}
+
+print(myFirstRFunc(1000))
